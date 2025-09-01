321219129fb491c0a8031bed1dd5fbfc58f67be7
YunWooJin <satoru31572@gmail.com>
2025-09-01 18:39:21 +0900

    과제 코드

--- PlayMaker(비공유).sql
+++ PlayMaker(비공유).sql
@@ -7,3 +7,12 @@ select student_name from tb_student where student_no in ('A513079', 'A513090', '
 select department_name , category from tb_department where capacity between 20 and 30;
 
 select professor_name from tb_professor where department_no is null;
+
+select student_name from tb_student where coach_professor_no is null;
+
+select distinct class_no from tb_class where preattending_class_no is not null;
+
+select distinct category from tb_department;
+
+select student_no, student_name, student_ssn from tb_student where student_no like '%02%'
+and substr(student_ssn, 8,1) = '2' and absence_yn = 'N';
