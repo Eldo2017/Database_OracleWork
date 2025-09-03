8245fa84c3b4b929178d9b9c3ed970e0af5e51db
YunWooJin <satoru31572@gmail.com>
2025-09-03 22:20:06 +0900

    문제 풀이

--- 03.1.서브쿼리_PlayMaker_계정.sql
+++ 03.1.서브쿼리_PlayMaker_계정.sql
@@ -51,6 +51,7 @@ select emp_id, emp_name, salary from employee where salary >= (select round(avg(
     - 일반 비교연산자 사용 가능
       ( =, !=, >, >=, <, <= )
     - 조인 또한 가능
+    - group by도 가능
 */
 
 -- 1) 전 직원의 평균 급여보다 급여를 더 적게 받는 사원들의 사원명, 직급코드, 급여 조회
@@ -84,3 +85,105 @@ join department on (dept_code = dept_id)
 where dept_code = (select dept_code from employee where emp_name = '김삼수')
 and emp_name <> '김삼수';
 
+-- 6) 부서별 급여합이 가장 큰 부서의 부서코드, 급여의 합 조회
+select dept_code, sum(salary) from employee
+group by dept_code having sum(salary) = (select max(sum(salary))
+from employee group by dept_code);
+
+--------------------------------------------------------------------------------
+/*
+    2. 다중행 서브쿼리(multi row subquery)
+    - 서브쿼리를 수행한 결과값이 여러 행일 때(여러 행 1열)
+    - in 서브쿼리 : 여러개의 결과값 중 한개라도 일치하는 값이 있다면 가져와라
+    - > any 서브쿼리 : 여러개의 결과값 중 한개라도 큰 값이 있을 경우 가져와라
+                     (여러개의 결과값 중에서 가장 작은값보다 클 경우)
+    - < any 서브쿼리 : 여러개의 결과값 중 한개라도 작은 값이 있을 경우 가져와라
+                     (여러개의 결과값 중에서 가장 큰값보다 작을 경우)
+    - all : 서브쿼리 값들 중 가장 큰값보다 더 큰 값을 얻어올 때 사용한다
+                     
+    - 비교대상 > ANY (값1, 값2, 값3)
+      (= 비교대상 > 값1 or 비교대상 > 값2 or 비교대상 > 값3)
+*/
+
+-- 1. 김영태 또는 한채린 사원과 같은 직급인 사원들의 사번, 사원명, 직급코드, 급여 조회
+------- 1) 김영태 또는 한채린이 어느 직급인가
+select job_code from employee where emp_name in ('김영태','한채린');
+
+------- 2) 두 사람과 같은 직급(J5, J7)인 사원들은 누구인가
+select emp_id, emp_name, job_code, salary from employee where job_code in ('J5','J7');
+
+------- 3) 위의 두 코드를 하나로 합치면?
+select emp_id, emp_name, job_code, salary from employee where job_code in (select job_code from employee where emp_name in ('김영태','한채린'));
+
+-- 2. 대리 직급임에도 과장 직급의 급여들 중, 최소 급여보다 많이 받는 사원의 사번, 사원명, 직급, 급여 조회
+------- 1) 과장 직급의 급여
+select salary from employee join job using(job_code) where job_name = '과장';  -- 결과값 : 3760000, 2200000, 2500000
+
+------- 2) 직급이 대리이며 위 급여의 목록값들 중 하나라도 큰 사원의 사번, 사원명, 직급, 급여
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '대리' and 
+salary > any(3760000, 2200000, 2500000);
+
+------- 3) 위 쿼리문을 하나로 표현!
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '대리' and 
+salary > any(select salary from employee join job using(job_code) where job_name = '과장');
+
+------- 4) 단일행 쿼리로도 된다
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '대리' and 
+salary > (select min(salary) from employee join job using(job_code) where job_name = '과장');
+
+-- 3. 차장 직급임에도 과장 직급의 급여보다 적게 받는 사원의 사번, 사원명, 직급, 급여 조회
+------- 1) 과장 직급의 급여
+select salary from employee
+join job using(job_code) where job_name = '과장';
+
+------- 2) 직급이 차장이며 위 급여의 목록값들 중 하나라도 적은 사원의 사번, 사원명, 직급, 급여
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '차장' and 
+salary < any(3760000, 2200000, 2500000);
+
+------- 3) 위 쿼리문을 하나로 표현!
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '차장' and 
+salary < any(select salary from employee join job using(job_code) where job_name = '과장');
+
+------- 4) 단일행 쿼리로도 된다
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '차장' and 
+salary < (select max(salary) from employee join job using(job_code) where job_name = '과장');
+
+-- 4. 과장 직급임에도 차장 직급의 사원들의 모든 급여보다 많이 받는 사원의 사번, 사원명, 직급, 급여 조회
+------- 1) 차장 직급의 급여
+select salary from employee
+join job using(job_code) where job_name = '차장';
+
+------- 2) 직급이 과장이며 위 급여의 목록값들 중 하나라도 큰 사원의 사번, 사원명, 직급, 급여
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '과장' and 
+salary > all(2800000, 2480000, 2490000, 1550000);
+
+------- 3) 위 쿼리문을 하나로 표현!
+select emp_id, emp_name, job_name, salary from employee
+join job using(job_code) where job_name = '과장' and 
+salary > all(select salary from employee
+join job using(job_code) where job_name = '차장');
+
+--------------------------------------------------------------------------------
+/*
+    2. 다중열 서브쿼리(multi column subquery)
+    - 서브쿼리를 수행한 결과값이 여러 열일 때(1행 여러열)
+*/
+-- 1. 홍만길 사원과 같은 부서코드, 같은 직급코드에 해당하는 사원들의 사원명, 부서코드, 직급코드, 입사일자를 구하라
+select emp_name, dept_code, job_code, hire_date from employee
+where dept_code = (select dept_code from employee where emp_name = '홍만길')
+and job_code = (select job_code from employee where emp_name = '홍만길');
+
+-- >> 이를 다중열 서브쿼리로!
+select emp_name, dept_code, job_code, hire_date from employee
+where (dept_code, job_code) = (select dept_code, job_code from employee where emp_name = '홍만길');
+
+-- 2. 김관철 사원과 같은 직급코드, 같은 사수를 가지고 있는 사원들의 사번, 사원명, 직급코드, 사수사번 조회
+select emp_id, emp_name, job_code, manager_id from employee
+where (job_code, manager_id) = (select job_code, manager_id from employee where emp_name = '김관철');
---
+++ 03.2.서브쿼리_PlayMaker_계정.sql
@@ -0,0 +1,69 @@
+-- SUBQUERY_연습문제
+-- 1. 70년대 생(1970~1979) 중 여자이면서 한씨인 사원의 사원명, 주민번호, 부서명, 직급명 조회
+select emp_name, emp_no, dept_title, job_name from employee
+join department on dept_code = dept_id
+join job using(job_code)
+where emp_no in(
+select emp_no from employee
+where substr(emp_no,1,2) between '70' and '79'
+and substr(emp_no,8,1)='2')
+and emp_name like '한%';
+-- 2. 나이가 가장 막내의 사번, 사원명, 나이, 부서명, 직급명 조회
+select emp_id, emp_name, extract(year from sysdate) - (1900 + to_number(substr(emp_no,1,2))) as 나이, 
+dept_title, job_name from employee
+join department on dept_code = dept_id
+join job using(job_code)
+where extract(year from sysdate) - (1900 + to_number(substr(emp_no,1,2))) 
+= (select min(extract(year from sysdate) - (1900 + to_number(substr(emp_no,1,2)))) from employee);
+-- 3. 이름에 '철'이 들어가는 사원의 사번, 사원명, 직급명 조회
+select emp_id, emp_name, job_name from employee
+join job using(job_code)
+where emp_name like '%철%';
+-- 4. 부서 코드가 D5이거나 D6인 사원의 사원명, 직급명, 부서코드, 부서명 조회
+select emp_name, job_name, dept_code, dept_title from employee
+join department on (dept_code = dept_id)
+join job using (job_code)
+where dept_code in ('D5','D6');
+-- 5. 보너스를 받는 사원의 사원명, 보너스, 부서명, 지역명 조회
+select emp_name, bonus, dept_title, local_name from employee
+join department on (dept_code = dept_id)
+join location on (location_id = local_code)
+where bonus is not null;
+-- 6. 모든 사원의 사원명, 직급명, 부서명, 지역명 조회
+select emp_name, job_name, dept_title, local_name from employee
+join department on (dept_code = dept_id)
+join job using(job_code)
+join location on (location_id = local_code);
+-- 7. 한국이나 일본에서 근무 중인 사원의 사원명, 부서명, 지역명, 국가명 조회
+select emp_name, dept_title, local_name, national_name from employee
+join department on (dept_code = dept_id)
+join location on (location_id = local_code)
+join national using (national_code)
+where national_name in ('한국', '일본');
+-- 8. 김무옥 사원과 같은 부서에서 일하는 사원의 사원명, 부서코드 조회
+select emp_name, dept_code from employee
+where dept_code = (select dept_code from employee where emp_name = '김무옥');
+-- 9. 보너스가 없고 직급 코드가 J4이거나 J7인 사원의 사원명, 직급명, 급여 조회 (NVL 이용)
+select emp_name, job_name, salary from employee
+join job using(job_code)
+where nvl(bonus, 0) = 0
+and job_code in ('J4','J7');
+-- 10. 퇴사 하지 않은 사람과 퇴사한 사람의 수 조회
+select count(case when end_date is null then 1 end) as "퇴사를 안 한 사람",
+count(case when end_date is not null then 1 end) as "퇴사를 한 사람"
+from employee;
+-- 안함 11. 보너스 포함한 연봉이 높은 5명의 사번, 사원명, 부서명, 직급명, 입사일, 순위 조회
+-- 12. 부서 별 급여 합계가 전체 급여 총 합의 20%보다 많은 부서의 부서명, 부서별 급여 합계 조회
+--	12-1. JOIN과 HAVING 사용
+select dept_title, sum(salary) as "부서별 급여합"
+from employee 
+join department on dept_code = dept_id 
+group by dept_title
+having sum(salary) > (select sum(salary)*0.2 from employee);
+--	안함 12-2. 인라인 뷰 사용      
+--	안함 12-3. WITH 사용
+-- 13. 부서명별 급여 합계 조회(NULL도 조회되도록)
+select nvl(dept_title,'부서없음') as 부서명, sum(salary) as 급여합 from employee
+left join department on dept_code = dept_id
+group by dept_title;
+-- 안함 14. WITH를 이용하여 급여합과 급여평균 조회
